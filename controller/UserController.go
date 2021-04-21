228d26dda0cc8515a49aae03347db2178644d028
peter <peterloginmanila@gmail.com>
2021-04-21 16:10:42 +0400

    Jwt配合中间件实现用户认证

--- controller/UserController.go
+++ controller/UserController.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
+	"log"
 	"net/http"
 )
 
@@ -86,7 +87,12 @@ func Login (ctx *gin.Context){
 		return
 	}
 	// 发放token
-	token := "1223"
+	token, err := common.ReleaseToken(user)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "系统异常"})
+		log.Printf("token generate error : %v", err)
+		return
+	}
 	// 返回结果
 	ctx.JSON(200, gin.H{
 		"code": http.StatusOK,
@@ -95,6 +101,10 @@ func Login (ctx *gin.Context){
 	})
 }
 
+func Info(ctx *gin.Context){
+	user, _ := ctx.Get("user")
+	ctx.JSON(http.StatusOK, gin.H{"code": 200, "data": gin.H{"user": user}})
+}
 func isTelephoneExist(db *gorm.DB, telephone string) bool {
 	var user model.User
 	db.Where("telephone = ?", telephone).First(&user)
